b84fc162fb9add4cfe299ff8d026c780a6ab2165
ma34s <ma34s.sss@gmail.com>
2014-01-15 01:43:05 +0900

    move to common

--- device.mk
+++ device.mk
@@ -15,13 +15,6 @@
 
 $(call inherit-product-if-exists, vendor/samsung/hltedcm/hltedcm-vendor.mk)
 
-
-#AOJP Recovery
-$(call inherit-product-if-exists, bootable/recovery/etc/aojp.mk)
-
-#AOJP Common
-$(call inherit-product-if-exists, vendor/aojp/config/aojp.mk)
-
 TARGET_NFC_TECH := nxp
 
 # Device Overlays
